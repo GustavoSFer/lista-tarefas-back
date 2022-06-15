cc2f45943f1b91b0d43a6af992a888833e2c6c8d
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-06-15 13:42:10 -0300

    criando arquivo de rotas

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import express from 'express';
+import route from './router';
 
 const app = express();
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => console.log(`Rodando na posta ${PORT}`))
+app.use(express.json());
+app.use(route);
+
+app.listen(PORT, () => console.log(`Rodando na posta ${PORT}`));
