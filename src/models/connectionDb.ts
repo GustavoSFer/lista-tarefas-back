c602a601776077720bf82d32fab19c7a55faa1c1
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-06-15 13:54:43 -0300

    fazendo a conexao com o bando em mongodb

---
+++ src/models/connectionDb.ts
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+mongoose.connect(process.env.DB_MONGO || 'mongodb://localhost:27017/task-db');
+
+const taskSchema = new mongoose.Schema({
+  task: String,
+  status: String,
+});
+
+const model =  mongoose.model('Task', taskSchema);
+
+export default model;
