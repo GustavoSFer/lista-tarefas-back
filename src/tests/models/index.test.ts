66e4b8551ac6df8cdd758331dfd53958cca8177c
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-06-22 11:17:06 -0300

    Test: Retornando dados quando tem info no banco

--- src/tests/models/index.test.ts
+++ src/tests/models/index.test.ts
@@ -8,11 +8,17 @@ describe('Models', () => {
   describe('# Find', () => {
 
     describe('Buscando informações no banco de dados', () => {
+      beforeEach(() => sinon.restore())
       it('Quando não existe dados deve retornar um array vazio', async () => {
         sinon.stub(model, 'find').resolves([]);
         const findTask = await taskModel.read();
         expect(findTask).to.be.empty;
-      })
-    })
-  })
-})
+      });
+      it('Quando tiver dados, deve retornar um array com as informações', async () => {
+        sinon.stub(model, 'find').resolves(RESULTASK);
+        const findTask = await taskModel.read();
+        expect(findTask).to.be.equal(RESULTASK);
+      });
+    });
+  });
+});
