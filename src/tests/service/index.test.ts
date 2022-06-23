43bbf85fe89fd1f13bc5dbee2e421a450c79137c
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-06-23 11:32:01 -0300

    Test: verificando a função read e o create

---
+++ src/tests/service/index.test.ts
@@ -0,0 +1,39 @@
+import sinon from 'sinon';
+import { expect } from 'chai';
+import taskModel from '../../models/index';
+import taskService from '../../service/index';
+import { removerTask, RESULTASK, taskBody, updatedTask } from '../mock';
+import { ITask } from '../../interfaces/index';
+
+describe('Serivces', () => {
+
+  describe('#Read', () => {
+    describe('Buscando informações no banco de dados', () => {
+      beforeEach(() => sinon.restore());
+      it('Quando não existe dados deve retornar um array vazio', async () => {
+        sinon.stub(taskModel, 'read').resolves([]);
+        const findTask = await taskService.read();
+        expect(findTask).to.be.empty;
+      });
+      it('Quando tiver dados, deve retornar um array com as informações', async () => {
+        sinon.stub(taskModel, 'read').resolves(RESULTASK as ITask[]);
+        const findTask = await taskService.read();
+        expect(findTask).to.be.equal(RESULTASK);
+      });
+    });
+  });
+
+
+
+  describe('#Create', () => {
+    describe('Criando Tasks no banco de dados', () => {
+      it('Deve retornar a task criada', async () => {
+        sinon.stub(taskModel, 'create').resolves(RESULTASK[0]);
+        const createTask = await taskService.create(taskBody);
+        expect(createTask).to.be.eqls(RESULTASK[0]);
+      });
+    });
+  });
+
+
+});
